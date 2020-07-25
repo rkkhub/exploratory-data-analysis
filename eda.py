04fbe87e4179e4a03715b59fc39446d283a1d89c
unknown <rkkmailbox@gmail.com>
2020-07-25 21:57:17 +0530

    uni/bi/multi-variate analysis

--- eda.py
+++ eda.py
@@ -1,43 +1,15 @@
-# detect the types of columns in a data frame
-# unique values, missing values
-# minimum value, Q1, median, Q3, maximum, range, interquartile range
-# like mean, mode, standard deviation, sum, median absolute deviation, coefficient of variation, kurtosis, skewness
-# Most frequent values
-# Histogram
-# highly correlated variables, Spearman, Pearson and Kendall matrices
-# Missing values matrix, count, heatmap, and dendrogram of missing values
-# Text analysis learns about categories (Uppercase, Space), scripts (Latin, Cyrillic), and blocks (ASCII) of text data
-# File and Image analysis extract file sizes, creation dates, and dimensions and scan for truncated images or those containing EXIF information
-
-
-# remove duplicates
-# drop irrelavant data/columns
-# handle missing values
-# handle outlier datapoints
-# relationship analysis - correlation matrix
-
-from bokeh.plotting import figure, output_file, show
 from pathlib import Path
-import bokeh
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 
 
-
 df = pd.read_csv(Path("data-spotify/data.csv"))
-df_artist = pd.read_csv(Path("data-spotify/data_by_artist.csv"))
-df_genres_a = pd.read_csv(Path("data-spotify/data_by_genres.csv"))
-df_genres_b = pd.read_csv(Path("data-spotify/data_w_genres.csv"))
-df_year = pd.read_csv(Path("data-spotify/data_by_year.csv"))
-df_super_genres = pd.read_json(Path("data-spotify/super_genres.json"))
-
-
-#-------------------------------------------------------------------- Prilmnary Analysis
+#--------------------------------------------------------------------------------------- Prilmnary Analysis
 """
 Objective of the prilmnary analysis is to understand the
-dataset with datatypes, missing values, duplicate entries,  
+dataset w.r.t datatypes and missing values 
 """
 
 df.drop_duplicates(inplace=True)
@@ -48,23 +20,21 @@ df.isna().sum()
 
 """
 [summary]
-    total columns: 19
-    total rows: 169909
-    total numerical data columns: 15
-    total categorical data columns: 4
-    columns with null value present: 0
-    total data in columns as 'na' - not applicable: 19
-    *column-artists data is a string of list of strings 
-    *column-release_date has only year others full date (YYYY-mm-dd)
-    *colums with datetime was inported as numerical (year) and string (release_date) data type
+    total features: 19
+    total datapoints: 169909
+    the dataset has  15 - continous and 4 - catogorecal features
+    the dataset has zero null and na values accross features
+    "artists" data is a string of list of strings 
+    "release_date" has mixed datapoints: year (YYYY) and date (YYYY-mm-dd), requires processing
+    colums with datetime were imported as numerical ("year") and string ("release_date") data type
 """
-df_cont = df.select_dtypes(include=np.number)                        # dividing the dataset into continuous and categorical dataframes
-df_cat = df.select_dtypes(include=object)
+df_cont = df.select_dtypes(include=np.number)                        # dataframe of continuous data
+df_cat = df.select_dtypes(include=object)                            # dataframe of categorical data
 
 
-#-------------------------------------------------------------------- Univariate EDA
+#--------------------------------------------------------------------------------------- Univariate EDA
 """
-To obtain information on values in a column w.r.t  weight, frequency and distribution
+To obtain information on values of a feature w.r.t weight, frequency and distribution
 
 Categorical Data
     -useful charts: barchart
@@ -86,11 +56,11 @@ df_cont.drop(["duration_ms"],axis=1, inplace=True)
 def chart_histograms():
     df_cont.hist(figsize=(20, 20))
     plt.show()
-chart_histograms()
 
-#-------------------------------------------------------------------- Bivariate EDA
+
+#--------------------------------------------------------------------------------------- Bivariate EDA
 """
-To obtain information on relation between between two columns
+To obtain information on relation between between two features
 
 Categorical to Categorical Relation
     -
@@ -100,7 +70,7 @@ Continuous to Continuous Relation
     -correlation
     -useful charts: heatmap, clustermap
 """
-def chart_corr():
+def chart_corr():                                                    # correlation between features
     plt.figure(figsize=(16, 8))
     sns.set(style="whitegrid")
     corr = df_cont.corr()
@@ -113,12 +83,12 @@ def chart_corr():
 """
 
 
-#-------------------------------------------------------------------- Multivariate EDA
+#--------------------------------------------------------------------------------------- Multivariate EDA
 """
-To obtain information on relation between between multiple columns/features
+To obtain information on relation between between multiple features
 
 """
-def chart_featuresOverTimes():
+def chart_featuresOverTimes():                                       # time series visualizations 
     plt.figure(figsize=(16, 8))
     sns.set(style="whitegrid")
     columns = ["acousticness","danceability","energy","speechiness","liveness","valence"]
@@ -135,6 +105,9 @@ def chart_featuresOverTimes():
 """
 
 
+
+# rate of songs added in spotify over the years
+# feature trend analysis over the decades
 
 
 
